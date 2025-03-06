acf5767eacadb24bb04de3e3d7290918c690936d
Soanpapdi2517 <152783492+Soanpapdi2517@users.noreply.github.com>
2025-03-06 08:11:36 +0530

    06/03/2025

---
+++ 4 Event Loop and Async Code/UserInput/post.js
@@ -0,0 +1,54 @@
+const { error } = require("console");
+const filesystem = require("fs");
+
+const userRequestHandler = (req, res) => {
+  console.log(req.url, req.method);
+  if (req.url.toLowerCase() === "/submit-details" && req.method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const fullBody = Buffer.concat(body).toString();
+      const params = new URLSearchParams(fullBody);
+      // const bodyObj = {}
+      // for(const [key, value] of params.entries()){
+      //   bodyObj[key] = value;
+      // }
+      const bodyObj = Object.fromEntries(params);
+      console.log(bodyObj);
+      filesystem.writeFile("user.csv", JSON.stringify(bodyObj), (error) => {
+        console.log("File is successfully written");
+      });
+      res.statusCode = 302;
+      res.setHeader("Location", "/");
+      return res.end();
+    });
+    // filesystem.writeFileSync("userfile.txt", "Sonu Yadav")
+  } else {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Details of User page</title></head>");
+    res.write("<body><h1>Welcome to enter your details</h1></body>");
+    res.write('<form action="/submit-details" method="POST">');
+    res.write(
+      `
+        <input type="text" name="username" placeholder="Enter Your Username here"><br>
+     `
+    );
+
+    res.write('<label for="male">Male</label>');
+    res.write('<input type="radio" id="male" name="gender" value="male">');
+    res.write('<label for="female">Female</label>');
+    res.write(
+      '<input type="radio" id="female" name="gender" value="female"><br><br>'
+    );
+    res.write('<input type="submit" value="submit">');
+    res.write("</form>");
+    res.write("</html>");
+    res.end();
+  }
+};
+
+module.exports = userRequestHandler;
--- 6 Introduction to Express/app.js
+++ 6 Introduction to Express/app.js
@@ -1,21 +1,25 @@
-const http = require("http");
+// core middleware
+
+
+//external middleware
 const express = require('express');
+// local middleware
+const userRequestHandler = require('./post');
 
 const app = express();
-app.use(("/"),(req, res, next)=> {
-  console.log("i'm in first middleware");
-  next();
+app.get(("/"),(req, res, next)=> {
+  console.log("i'm in first middleware", req.url, req.method);
+  res.send("<h1>Express</h1>");
 })
-app.use(("/"),(req, res, next)=> {
-  console.log("i'm in another middleware");
-  res.send("<p>This is my / of Another Express program</p>");
-})
-app.use(("/home"),(req, res, next)=> {
+app.use(("/submit-details"),(req, res, next)=> {
   console.log("i'm in second middleware");
   res.send("<p>This is my Home of first Express program</p>")
 })
-const server = http.createServer(app);
+app.use(("/"),(req, res, next)=> {
+  console.log("i'm in another middleware", req.url, req.method);
+  res.send("<p>This is my / of Another Express program</p>");
+})
 const PORT = 3001;
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
