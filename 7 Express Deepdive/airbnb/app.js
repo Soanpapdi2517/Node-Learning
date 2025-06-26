a172381a5db9472b4f66f9a13c50faa7398b3c5a
Sonu Yadav <sy239370@gmail.com>
2025-06-26 20:48:44 +0530

    26-06-2025

--- 7 Express Deepdive/airbnb/app.js
+++ 7 Express Deepdive/airbnb/app.js
@@ -2,11 +2,13 @@
 const express = require("express");
 const app = express();
 //Local Module
+
 app.use((req, res, next) => {
   console.log(req.method, req.url);
   next();
 });
-app.get("/add-home", (req, res, next) => {
+app.use(express.urlencoded());
+app.get("/host/add-home", (req, res, next) => {
   res.send(`<h1>Register Your Home here:</h1>
     <form action='/add-home' method='post'>
     <input type='text' name='houseName' placeholder='Enter Your House Name'/>
@@ -14,13 +16,16 @@ app.get("/add-home", (req, res, next) => {
     <input type='submit' value='Register'/>
     </form>`);
 });
-app.post("/add-home", (req, res, next) => {
+app.post("/host/add-home", (req, res, next) => {
+  console.log(req.body);
   res.send(`<h1>Home Registered Successfully</h1>
-    <a href='/add-home'>Go to Home</a>`);
+    <h2>Home available</h2>
+    <h3>${req.body.houseName}</h3>
+    <a href='/host/add-home'>Go to Home</a>`);
 });
 app.get("/", (req, res, next) => {
   res.send(`<h1>Response Recieved</h1>
-    <a href='/add-home'>Go to Home</a>`);
+    <a href='/host/add-home'>Go to Home</a>`);
 });
 
 const PORT = 2004;
