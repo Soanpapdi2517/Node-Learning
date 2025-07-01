588f28a1a51fb464933e26a8b7917f6ed214a473
Sonu Yadav <sy239370@gmail.com>
2025-07-01 10:30:06 +0530

    1 /07/ 2025 - 1

--- 13 MVC/Models/singleHome.js
+++ 13 MVC/Models/singleHome.js
@@ -1,10 +1,13 @@
+const path = require('path');
+const fs = require('fs')
+
+//local Path
+const rootDir = require('../utils/PathUtils');
 //Fake DataBase
 const registeredHomes = [];
 
 module.exports = class Home{
     constructor(houseName, price, location, rating, imgURL){
-        //Taking arugment data to create a new object with key houseName 
-        // and value which will be given while creating instance of this class
         this.houseName = houseName;
         this.price = price;
         this.location =  location;
@@ -12,16 +15,31 @@ module.exports = class Home{
         this.imgURL = imgURL;
     }
     save(){
-        //this means new object which is gonna created by given argument
-        //this function will be called with the instance of Home Class
         registeredHomes.push(this);
+        const savaDataPath = path.join(rootDir, "data", "homeData.json");
+        fs.writeFile(savaDataPath, JSON.stringify(registeredHomes), err => {
+            console.log("File written concluded", err);
+        })
     }
 
-    //by creating Static we can call it by Home class using . operator
-    //why didn't we did it by instance of it then we had to give data also which is not possible for every function
-    //this function will be called with the instance of Home Class
+
     static fetchAll(){
         return registeredHomes;
     }
 
 }
+
+
+
+        //Taking arugment data to create a new object with key houseName 
+        // and value which will be given while creating instance of this class
+
+
+                //this means new object which is gonna created by given argument
+        //this function will be called with the instance of Home Class
+
+
+    //by creating Static we can call it by Home class using . operator
+    //why didn't we did it by instance of it then we had to give data also 
+    //which is not possible for every function
+    //this function will be called with the instance of Home Class
